b3d801182d90cae9b5d105e9a738be232c742bd4
Jade Abegail <jadeabegaild@gmail.com>
2024-12-01 20:56:22 +0800

    napindot

--- routes/web.php
+++ routes/web.php
@@ -8,28 +8,3 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        $vehicles = Vehicle::all();
-        return view('dashboard', ['vehicles' => $vehicles]);
-    })->name('dashboard');
-
-    Route::middleware(['role:admin'])->group(function () {
-        Route::get('/admin', function () {
-            return 'Admin Dashboard';
-        });
-    });
-    
-    Route::middleware(['role:customer'])->group(function () {
-        Route::get('/customer', function () {
-            return 'Customer Dashboard';
-        });
-    });
-});
-
-Auth::routes();
-
-Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])
-    ->middleware('auth')
-    ->name('dashboard');
